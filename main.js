edde63746288f082ac9c896a960efb292390a301
Vinicius Sviatovski Aragão <116413989+VinicusAragao@users.noreply.github.com>
2022-10-28 11:53:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 window.addEventListener('pointermove', move)
 let x
 let y
+let onLinks = false
 
 function move(e){
-	x = e.clientX
-	y = e.pageY
-	createPart()
+	if(!onLinks){
+		x = e.clientX
+		y = e.pageY
+		createPart()
+	}
 }
 function createPart(){
 	const body = document.getElementsByTagName('body')[0]
 	let el = document.createElement('div')
 	el.setAttribute('class', 'particle')
-	el.style.marginLeft = x + 'px'
+	el.style.marginLeft = (x - 3) + 'px'
 	el.style.marginTop = y + 'px'
 	body.appendChild(el)
 	setTimeout(deletePart, 300)
@@ -22,3 +25,14 @@ function deletePart(){
 		parts[0].remove()
 	}
 }
+window.addEventListener('load', () =>{
+	const links = document.getElementsByClassName('links')
+	for(let i = 0; i < links.length; i++){
+		links[i].addEventListener('mouseover', linkIn =>{
+			onLinks = true
+		})
+		links[i].addEventListener('mouseout', linkOut =>{
+			onLinks = false
+		})	
+	}
+})
