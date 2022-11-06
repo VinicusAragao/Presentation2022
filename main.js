01c6876386896fd492d8f850e42f6fa7a1dd75ea
Vinicius Sviatovski Aragão <116413989+VinicusAragao@users.noreply.github.com>
2022-11-05 22:20:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ let animDuration
 let eraseble
 let setTimeoutsId = []
 
+let laserWidth
+let laserColor
+let laserBlocked = false
+
 const body = document.getElementsByTagName('body')[0]
 const links = document.getElementsByClassName('links')
 const circles = document.getElementsByClassName('circles')
@@ -12,27 +16,29 @@ const logo = document.getElementById('logoLink')
 const parts = document.getElementsByClassName('particles')
 const input = document.getElementsByTagName('input')
 
-const rules = document.styleSheets[1].cssRules
-
 function changeAnimation(itself,name,duration,erase){
 	deleteAll()
 	state = itself.dataset.state
+
 	window.removeEventListener('pointerdown', pressedDown)
 	window.removeEventListener('pointerdown', move)
 	window.removeEventListener('pointerup', pressedUp)
 	window.removeEventListener('pointermove', move)
+	window.removeEventListener('pointermove', moveAim)
+
 	for(let i = 0; i < setTimeoutsId.length; i++){
 		clearTimeout(setTimeoutsId[i])
 	}
 
 	if(state === 'inactive'){
 		if(name === 'laser'){
+			window.addEventListener('pointermove', moveAim)
 			window.addEventListener('pointerdown', move)
 			window.addEventListener('pointerdown', pressedDown)
 			window.addEventListener('pointerup', pressedUp)
 		}
 		else{
-			window.addEventListener('pointermove', move)
+
 		}
 		animName = name
 		animDuration = duration
@@ -49,13 +55,17 @@ function changeAnimation(itself,name,duration,erase){
 }
 
 function move(e){
-	x = e.clientX
-	y = e.pageY
-	createPart()
+	if(!laserBlocked){
+		x = e.clientX
+		y = e.pageY
+		createPart()
+	}
 }
 function pressedDown(){
-	body.classList.add('noSelect')
-	window.addEventListener('pointermove', move)
+	if(!laserBlocked){
+		body.classList.add('noSelect')
+		window.addEventListener('pointermove', move)
+	}
 }
 function pressedUp(){
 	body.classList.remove('noSelect')
@@ -64,8 +74,18 @@ function pressedUp(){
 function createPart(){
 	let el = document.createElement('div')
 	el.setAttribute('class', `${animName} particles`)
-	el.style.marginLeft = (x - 3) + 'px'
-	el.style.marginTop = y +'px'
+
+	if(animName === 'laser'){
+		el.style.backgroundColor = laserColor
+		el.style.width = laserWidth + 'px'
+		el.style.marginLeft = (x - (laserWidth/2)) + 'px'
+		el.style.marginTop = (y - (laserWidth/2)) +'px'
+	}
+	else{
+		el.style.marginLeft = (x - 3) + 'px'
+		el.style.marginTop = y +'px'
+	}
+
 	body.appendChild(el)
 	if(eraseble){
 		setTimeoutsId.push(setTimeout(deletePart, animDuration))
@@ -81,22 +101,55 @@ function deleteAll(){
 		parts[0].remove()
 	}
 }
+function createAim(){
+	let aim = document.createElement('div')
+	aim.setAttribute('class', `laserAim particles`)
+	aim.style.width = laserWidth + 'px'
+	aim.style.backgroundColor = laserColor
+	aim.style.marginLeft = (x - (laserWidth/2)) + 'px'
+	aim.style.marginTop = (y - (laserWidth/2)) +'px'
+	body.appendChild(aim)
+	console.log(body)
+}
+function moveAim(){
+	let aim = document.getElementsByClassName("laserAim")[0]
+	console.log(aim)
+	aim.style.marginLeft = (x - (laserWidth/2)) + 'px'
+	aim.style.marginTop = (y - (laserWidth/2)) +'px'
+}
+
+//On Load 
+
+window.addEventListener('load', () => {
+	blockLaser(circles)
+	blockLaser(links)
+	blockLaser(logo)
+	blockLaser(input)
+
+	changeLaserProperties('width', input[0])
+	changeLaserProperties('background-color', input[1])
+})
+
+function blockLaser(elements){
+	for(let i = 0; i < elements.length; i++){
+		elements[i].addEventListener('pointerover', dontShowAnimation)
+		elements[i].addEventListener('pointerleave', nowShowAnimation)
+	}
+}
+function dontShowAnimation(){
+	laserBlocked = true
+}
+function nowShowAnimation(){
+	laserBlocked = false
+}
+
 function changeLaserProperties(type,input){
 	newValue = input.value
 
 	if(type === 'width'){
-		newValue += 'px'
+		laserWidth = newValue
 	}
-
-	for(let i = 0; i < rules.length; i++) {
-  		if(rules[i].selectorText === '.laser') {
-    		laserRules = rules[i];
-  		}
+	else if(type === 'background-color'){
+		laserColor = newValue
 	}
-	laserRules.style.setProperty(type, newValue)
-	console.log(laserRules)
 }
-window.addEventListener('load', () => {
-	changeLaserProperties('width', input[0])
-	changeLaserProperties('background-color', input[1])
-})
